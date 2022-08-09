8eb245556766f8d6345ddfca8d0a48a475a5f2a1
rz65hege <rz65hege@studserv.uni-leipzig.de>
2022-08-10 01:35:46 +0200

    Test view

---
+++ cookbook/prediction.py
@@ -0,0 +1,17 @@
+
+from cookbook.forms import Recipe
+
+
+
+
+
+
+class Prediction():
+
+    working_time = 0
+
+    def load_prediction(self):
+        return 100
+
+    def __str__(self):
+        return self.text
