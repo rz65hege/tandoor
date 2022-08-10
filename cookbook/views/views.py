f7333deadbd434e9958c2da59f1a447b1ee7a944
rz65hege <rz65hege@studserv.uni-leipzig.de>
2022-08-10 15:49:25 +0200

    Test view

--- cookbook/views/views.py
+++ cookbook/views/views.py
@@ -179,9 +179,8 @@ def recipe_view(request, pk, share=None):
 
 def feedback(request):
     url = settings.API_URL + 'feedback/'
-    payload = {'ingredients': "rise,something_else",'time':'100'}
 
-    x = request.POST(url, payload = {'ingredients': 'rise,something_else','time':'100'})
+    x = request.POST(url, payload = ['ingredients': 'rise,something_else','time':'100'])
 
     # if request.method == "POST":
     #     attempt_num = 0  # keep track of how many times we've retried
