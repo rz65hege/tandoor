474052b1706f11a080dafd4bbf7630c7c4a18a4a
rz65hege <rz65hege@studserv.uni-leipzig.de>
2022-08-11 16:43:30 +0200

    Test view

--- cookbook/views/views.py
+++ cookbook/views/views.py
@@ -204,7 +204,7 @@ def recipe_view(request, pk, share=None):
 
         client = coreapi.Client()
         #schema = client.get('http://127.0.0.1:8000/prediction/?ing=' + ','.join(food))
-        schema = client.get(settings.API_URL + 'prediction/?ing=' + ','.join(food))
+        schema = client.get(settings.API_URL + 'prediction_lite/?ing=' + ','.join(food))
 
         food = get_prediction(pk)
         prediction = {'time' : next(iter(schema.values())), 'ingredients': food}
