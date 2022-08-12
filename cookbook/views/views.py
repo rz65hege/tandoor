edf7c07056bb9d457a9954fca6346c2a028bb085
rz65hege <rz65hege@studserv.uni-leipzig.de>
2022-08-12 22:52:26 +0200

    Test view

--- cookbook/views/views.py
+++ cookbook/views/views.py
@@ -152,7 +152,7 @@ def feedback(request):
     time = request.GET.get('time')
     #ingredients = ','.join(request.GET.get('ingredients'))
     #pk = request.GET.get('pk')
-    pk = 1
+    pk = request.GET.get('pk')
 
     ingredients = Ingredient.objects.filter(unit=pk)
     recipe = Recipe.objects.filter(pk=pk)
